0e540a6727380c4a0148dae4977b462fc7bf1df3
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2026-04-18 00:09:47 +0300

    some archetecture

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <div className="bg-blue-500 h-20 w-20">
+        
+      </div>
+    </div>
+  );
+}
