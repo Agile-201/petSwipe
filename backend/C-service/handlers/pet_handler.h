0eca56cb8c2a47c4c8ef2f2150e6f3669516fbe4
Strees111 <d4nis30072005@yandex.ru>
2026-04-19 09:59:41 +0300

    fixed bugs

--- backend/C-service/handlers/pet_handler.h
+++ backend/C-service/handlers/pet_handler.h
@@ -17,12 +17,10 @@ struct Pet {
 class PetController : public HttpController<PetController> {
 public:
 	METHOD_LIST_BEGIN
-		// Методы доступны без авторизации
 		ADD_METHOD_TO(PetController::getFeed, "/api/feed", Get);
 		ADD_METHOD_TO(PetController::getList, "/api/pets/list", Get);
 		ADD_METHOD_TO(PetController::getByID, "/api/pets/{id}", Get);
 
-		// Методы требуют авторизации (проверяем в коде)
 		ADD_METHOD_TO(PetController::create, "/api/pets/create", Post);
 	METHOD_LIST_END
 
